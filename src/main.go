838be27ab1e6ec2e8366fddd1b2f45135ff5adc1
Nishant Gupta <39030732+nishgupta29@users.noreply.github.com>
2020-01-22 08:29:28 -0500

    Adding comments for understanding

--- src/main.go
+++ src/main.go
@@ -1,14 +1,18 @@
 package main
 
+#Importing required modules
+
 import (
 	"os"
 
 	"github.com/shomali11/slacker"
 )
+#Request writer
 
 func handle(request *slacker.Request, response slacker.ResponseWriter) {
 	response.Reply("Hey!")
 }
+#Connecting to slack and providing a response
 
 func main() {
 	bot := slacker.NewClient(os.Getenv("API_TOKEN"))
